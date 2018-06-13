c802bc9c9732b191be7cb9a8dc2f92c419623879
Draker21 <drak3r21@hotmail.com>
2018-06-13 16:07:19 +0200

    Ajout de la structure MVC et ré-organisation des fichiers

--- app/controller/ErrorController.php
+++ app/controller/ErrorController.php
@@ -1,10 +1,8 @@
 <?php
-class ErrorController extends Controller {
 
+class ErrorController extends Controller {
     public function error404(){
         $template = $this->twig->loadTemplate('/Error/404.html.twig');
-        echo $this->twig->render($template, array(
-            
-        ));
+        echo $template->render(array());
     }
 }
