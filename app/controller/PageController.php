77b7c06faf3e894671636b28f5099571a34ab431
Draker21 <drak3r21@hotmail.com>
2018-06-14 16:55:49 +0200

    Modification du view

--- app/controller/PageController.php
+++ app/controller/PageController.php
@@ -1,8 +1,11 @@
 <?php
 
 class PageController extends Controller {
-
+/* $recentMeme = Meme::displayLastGeneratedMeme(); */
     public function index(){
+        
+
+        
         $template = $this->twig->loadTemplate('/Page/index.html.twig');
         echo $template->render(array());
     }
